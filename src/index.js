a216cd3073bc8d283619906b6808d7e9d3a98780
Pizzaman333 <andrijmalchevv369@gmail.com>
2024-07-06 13:42:12 +0300

    fix modal doctor page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import "./js/log-in"
-import "./js/registration"
-import "./js/mobile-menu"
-import "./js/servise-modal"
+import "./js/log-in";
+import "./js/registration";
+import "./js/mobile-menu";
+import "./js/servise-modal";
+import "./js/call";
+import "./js/doctor";
